f6a28c39b43af135b5bc69b025104b4eb68c5bdb
Maxwell <lifeimax@gmail.com>
2022-09-09 06:51:12 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,12 +11,12 @@ int main(void)
 	int for_integer;
 	long int for_long;
 	long long int for_longlong;
-	float for_float;	
-
+	float for_float;
+	
 	printf("Size of a char: %lu byte(s)\n", sizeof(for_character));
 	printf("Size of an int: %lu byte(s)\n", sizeof(for_integer));
-	printf("Size of a long: %lu byte(s)\n", sizeof(for_long));
-	printf("Size of a long long: %lu byte(s)\n", sizeof(for_longlong));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(for_long));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(for_longlong));
 	printf("Size of a float: %lu bytes(s)\n", sizeof(for_float));
 	return (0);
 }
