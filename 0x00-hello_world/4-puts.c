f3267070dabfe8e538c40dddb4f49dea2459deed
Maxwell <lifeimax@gmail.com>
2022-09-09 01:18:17 -0700

    My commit for task 5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * includes brings the standard input out library for c
+ * Returns :o when we specific int and writes a text
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,3 +1,4 @@
 This file preprocesses main c file
 2-assembler only compiles main c file
 3-name creates a cisfun file which is then saved on $CFILE
+4-puts.c prints a text with the function puts
