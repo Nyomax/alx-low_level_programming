27194bb0a2240ca59a3563ccf8b711519c3c4ad6
Maxwell <lifeimax@gmail.com>
2022-09-09 02:48:57 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * This code links the main library
+ * prints using a function puts "Programming is like building a multilingual puzzle
+ * returns 0 upon success
+ */
+int main(void){
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
