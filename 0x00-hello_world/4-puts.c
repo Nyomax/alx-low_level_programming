44d5f1e5586f9598a7e165a2c468404805c61e22
Maxwell <lifeimax@gmail.com>
2022-09-09 03:34:45 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,10 @@
  * main-executes the text in the function puts
  * Description:using the main function
  * This program prints "Programming is like building a multilingual puzzle
- * return:0 upon success
+ * Return:0 upon success
  */
-int main(void){
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
