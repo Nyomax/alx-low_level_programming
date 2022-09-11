d682af0c05e2bb2f30bfcf62e4455e8037110507
Maxwell <lifeimax@gmail.com>
2022-09-11 09:49:48 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main-executes the function putchar
+ * Description:program that prints all possible combinations of single-digit
+ * numbers
+ * Return:0
+ */
+int main(void)
+{
+	int b = 0;
+		
+		while (b < 10)
+		{
+			putchar(b + '0');
+			if (b < 9);
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			b++;
+		}
+		putchar('\n');
+	return (0);
+}
