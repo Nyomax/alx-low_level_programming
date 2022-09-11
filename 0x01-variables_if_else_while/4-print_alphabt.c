5180291db014bd4f73a86d01aa82dc60e54c4ab5
Maxwell <lifeimax@gmail.com>
2022-09-11 06:04:38 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main-executes the function putchar
+ * Description:program that prints the alphabet in lowercase,
+ * followed by a new line.
+ * Return:0
+ */
+int main(void)
+{
+	char ch, e, q;
+	e = 'e';
+	q = 'q';
+		for (ch = 'a'; ch <= 'z'; ch++)
+		if (ch != e && ch != q)
+		putchar(ch);
+		putchar('\n');
+	return (0);
+}
