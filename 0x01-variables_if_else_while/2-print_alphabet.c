e748466f046a1c177c16058efdc7737da408bd34
Maxwell <lifeimax@gmail.com>
2022-09-10 20:16:57 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main:runs the function putchar
+ * description:program prints the alphabet in lowercase, followed by a new line
+ * Return:0 on success
+ */
+int main(void)
+{
+	char cha = a;
+		for (cha = 'a'; cha = 'z'; cha++)
+		putchar(cha);
+		putchar('\n');
+	return (0);
+}
+
