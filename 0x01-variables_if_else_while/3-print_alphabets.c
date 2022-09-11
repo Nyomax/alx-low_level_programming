59ee5006c804540ee6f9d1b53713f035b097b9b7
Maxwell <lifeimax@gmail.com>
2022-09-11 04:57:22 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,6 +10,8 @@ int main(void)
 	char ch = 'a';
 		for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
+		for (ch = 'A'; ch <= 'Z'; ch++);
+		putchar(ch);
 		putchar('\n');
-	return 0;
+	return (0);
 }
