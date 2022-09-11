1e115a6d9b7085704d56579c2dff57204378e98a
Maxwell <lifeimax@gmail.com>
2022-09-11 04:28:13 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,7 +17,7 @@ int main(void)
 	printf("Size of an int: %d byte(s)\n", sizeof(in));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(l_int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(ll_int));
-	printf("Size of a float:%ld byte(s)\n", sizeof(f));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
 
