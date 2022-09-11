bbbe92004b90427b671fdb94b3e3d2e761361401
Maxwell <lifeimax@gmail.com>
2022-09-11 06:10:22 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,10 +8,10 @@
 int main(void)
 {
 	char ch, e, q;
+
 	e = 'e';
 	q = 'q';
-
-		for (ch = 'a'; ch <= 'z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 		if (ch != e && ch != q)
 		putchar(ch);
 		putchar('\n');
