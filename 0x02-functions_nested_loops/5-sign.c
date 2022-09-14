b8408d8423668036af83ff7ff5abcea63d4ba14f
Maxwell <lifeimax@gmail.com>
2022-09-14 09:57:06 -0700

    ‘5-sign.c’

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,12 +11,14 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-       	else if (n = 0)
+       	else if (n == 0)
 	{
 		_puchar('0');
 		return (0);
 	}
 	else
+	{
 		_putchar('-');
 		return (-1);
+	}
 }
