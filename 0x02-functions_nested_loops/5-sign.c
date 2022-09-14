2bcceb9a61e55500daad298d02dada393d0dbec1
Maxwell <lifeimax@gmail.com>
2022-09-14 09:33:19 -0700

    ‘5-sign.c’

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_sign - a function that prints the sign of a number
+ * @c: The argument of the function
+ * Return:0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if else (n = 0)
+	{
+		_puchar('0');
+		return (0);
+	}
+	else
+		_putchar('-');
+		return (-1);
+}
