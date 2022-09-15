f564a702ef4dc6c643d032239000c054d54bb335
Maxwell <lifeimax@gmail.com>
2022-09-15 03:11:43 -0700

    ‘9-times_table.c’

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -24,12 +24,7 @@ void times_table(void)
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
 			}
-			if (j < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
 		}
-	       	_putchar('\n');
+		_putchar('\n');
 	}
 }
