2f336d31a774f80638ec89d6a557bdd7e40e5336
Maxwell <lifeimax@gmail.com>
2022-09-14 18:03:05 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,7 +9,7 @@ void jack_bauer(void)
 	int hours;
 	int mins;
 
-	for (hours = 0; hours <= 24; hours++)
+	for (hours = 0; hours <= 23; hours++)
 	{
 		for (mins = 0; mins <= 59; mins++)
 		{
@@ -20,5 +20,5 @@ void jack_bauer(void)
 			_putchar((mins % 10) + '0');
 		       	_putchar('\n');
 		}
-	}		
+	}
 }
