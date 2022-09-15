d6a99e05a5b58f953ff9d9815c8e27fd1cde91bc
Maxwell <lifeimax@gmail.com>
2022-09-14 21:58:01 -0700

    ‘11-print_to_98.c’

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /*
- * add - a function that adds two integers and returns the result
+ *add - a function that adds two integers and returns the result
  *@a:first argument
  *@b:second argument
- * Return:addition result
+ *Return:addition result
  */
 int add(int a, int b)
 {
