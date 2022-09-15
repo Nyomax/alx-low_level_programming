34ef803011d8578b68881b2e462be48022c156e6
Maxwell <lifeimax@gmail.com>
2022-09-14 22:26:53 -0700

    ‘9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,12 +9,13 @@ void times_table(void)
 {
 	int i,j,k;
 
-	for (i=0 ; i<10 ; i++)
+	for (i = 0; i < 9; i++)
 	{
-		for (j=0 ; j<10 ; j++)
+		for (j = 0; j < 9; j++)
 		{
 			k = i*j;
-			if(k<=9)
+
+			if (k <= 9)
 			{
 				_putchar(k + '0');
 			}
@@ -23,7 +24,7 @@ void times_table(void)
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
 			}
-			if(j<9)
+			if (j < 9)
 			{
 				_putchar(',');
 				_putchar(' ');
