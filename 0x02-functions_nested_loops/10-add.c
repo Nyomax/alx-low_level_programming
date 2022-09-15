ded1f91dc80b04a7893137a60411594bdef82162
Maxwell <lifeimax@gmail.com>
2022-09-14 21:22:41 -0700

    ‘10-add.c’

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/*
+ * add - a function that adds two integers and returns the result
+ *@a:first argument
+ *@b:second argument
+ * Return:addition result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
