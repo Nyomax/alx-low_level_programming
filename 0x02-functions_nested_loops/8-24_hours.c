6a3b374b16f63b563ce740bd7d2868d5335bfb21
Maxwell <lifeimax@gmail.com>
2022-09-14 17:38:00 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include “main.h”
+/**
+ *jack_bauer - a function that prints every minute of the day of Jack Bauer,
+ *starting from 00:00 to 23:59  
+ *Return:0
+ */
+void jack_bauer(void)
+{
+	int hours; mins;
+
+	for (hours = 0; hours <= 24; hours++)
+	{
+		for (mins = 0; mins <= 59; mins++)
+		{
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar(':');
+			_putchar((mins / 10) + '0');
+			_putchar((mins % 10) + '0');
+		       	_putchar('\n');
+		}
+	}		
+}
