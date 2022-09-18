09ca88bd9649bf2512bf7502153827aa028216f9
Maxwell <lifeimax@gmail.com>
2022-09-17 22:15:10 -0700

    ‘100-print_comb3.c’

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,8 +15,8 @@ int main(void)
 		num2 = num1 + 1;
 		while (num2 <= 9)
 		{
-			putchar(num1);
-			putchar(num2);
+			putchar(num1 + '0');
+			putchar(num2 + '0');
 			if (num1 != 8 || num2 != 9)
 			{
 				putchar(',');
