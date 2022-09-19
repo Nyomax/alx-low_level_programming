3dddb39699a40cbfa61cdf6fb94363a4aba9bbaf
Maxwell <lifeimax@gmail.com>
2022-09-18 22:02:18 -0700

    ‘4-print_most_numbers.c’

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,10 +8,12 @@ void print_most_numbers(void)
 {
 	int num = 0;
 
-	while (n <= 9)
+	while (num <= 9)
 	{
 		if (num != 2 && num != 4)
+		{
 			_putchar(num + '0');
+		}
 		num += 1;
 	}
 	_putchar('\n');
