70ef56decf8ebe5d8d22615d918a5b628a94eb16
Maxwell <lifeimax@gmail.com>
2022-09-20 00:11:55 -0700

    ‘100-prime_factor.c’

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - finds and prints the largest prime factor of the number
+ * 612852475143
+ * Return: 0.
+ */
+int main(void)
+{
+	long div = 2;
+	long num = 612852475143;
+	long larg_prim = 0;
+
+	while (num != 1)
+	{
+		if (num % div == 0)
+			num = num / div;
+			larg_prim = div;
+
+		div++;
+	}
+	printf("%ld\n", larg_prim);
+	return (0);
+}
