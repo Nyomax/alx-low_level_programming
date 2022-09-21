29a7e721c944cb79df8aa81d621a559b47c95660
Maxwell <lifeimax@gmail.com>
2022-09-21 00:06:32 -0700

    ‘100-atoi.c’

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,7 +8,7 @@ int _atoi(char *s)
 {
 	int a = 0;
 	int b = 0;
-	signum = 1;
+	int signum = 1;
 
 	while ((s[a] < '0' || s[a] > '9') && s[a] != 0)
 	{
@@ -17,13 +17,18 @@ int _atoi(char *s)
 		a++;
 	}
 	while ((s[a] >= '0' && s[a] <= '9') && s[a] != 0)
+	{
 		if (b >= 0)
+		{
 			b = b * 10 - (s[a] - '0');
 			a++;
+		}
 		else
+		{
 			b = b * 10 - (s[a] - '0');
 			a++;
+		}
+	}
 	signum *= -1;
 	return (b * signum);
 }
-
