6a696e41558308266722c1a4c50f76aa1df026dd
Maxwell <lifeimax@gmail.com>
2022-09-22 09:10:04 -0700

    ‘5-string_toupper.c’

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * :string_toupper - function that changes all lowercase letters
+ * string_toupper - function that changes all lowercase letters
  * of a string to uppercase.
  * @ch:Pointer to char
  * Return: nothing.
