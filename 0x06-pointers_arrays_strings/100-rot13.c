e685ab3dffe4a1905e0792dec4d27c579e926551
Maxwell <lifeimax@gmail.com>
2022-09-22 23:17:00 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rot13 - Function that reverses the content of an array of integers.
+ * @str: s is the array
+ * Return: always 0.
+ */
+char *rot13(char *str)
+{
+	int i = 0, k;
+	char s[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char s1[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (; str[i] != '\0'; i++)
+	{
+		for (k = 0; k <= 51; k++)
+		{
+			if (s[k] == str[i])
+			{
+				str[i] = s1[k];
+			}
+		}
+	}
+}
+
