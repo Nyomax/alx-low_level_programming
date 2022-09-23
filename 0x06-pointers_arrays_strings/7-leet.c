a426e323f93028bab14c8b7ad6487831aa95a15b
Maxwell <lifeimax@gmail.com>
2022-09-22 21:23:02 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,19 +7,17 @@
 char *leet(char *s)
 {
 	int i = 0;
-       	int j = 0;
-	char *l = "aAeEoOlLtT";
-	char *n = "4433001177";
+	int j = 0;
+	char array_leet[] = {'4', '3', '1', '0', '7'};
+	char array_up[] = {'A', 'E', 'L', 'O', 'T'};
+	char array_low[] = {'a', 'e', 'l', 'o', 't'};
 
-	while (*(s + i) != '\0')
+	while (s[i] != '\0')
 	{
-		while (*(l + j) != '\0')
+		for (j = 0; j < 5; j++)
 		{
-			if (*(s + i) == *(l + j))
-			{
-				*(s + i) = *(n + j);
-			}
-			j++;
+			if (s[i] == array_low[j] || s[i] == array_up[j])
+				s[i] = array_leet[j];
 		}
 		i++;
 	}
