2fb2f70010feabb4ee19e342726758279a5dbc8c
Maxwell <lifeimax@gmail.com>
2022-09-28 18:48:10 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,17 +7,21 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int j, i;
+	unsigned int i, j;
 	
-	for (j = 0; s[j] != '\0'; j++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (i = 0; accept[i] != '\0'; i++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (s[j] == accept[i])
+			if (s[i] == accept[j])
+			{
 				break;
+			}
 		}
-		if (!(accept[i]))
+		if (!(accept[j]))
+		{
 			break;
+		}
 	}
-	return (j);
+	return (i);
 }
