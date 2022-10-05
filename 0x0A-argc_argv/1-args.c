bcc38677717c72765d6cf6523abf5b1d2aad6318
Maxwell <lifeimax@gmail.com>
2022-10-05 15:30:05 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main - function that prints the number of arguments passed into it.
+ * @argc: argument count
+ * @argv: argunment vector
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		argv = argv;
+		printf("%d\n", argc - 1);
+	}
+	return (0);
+}
