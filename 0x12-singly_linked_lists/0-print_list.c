39e55233bde8d21ab779550b6717c817e58973c4
Maxwell <lifeimax@gmail.com>
2022-10-20 20:36:24 -0700

    file 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,13 +11,9 @@ size_t print_list(const list_t *h)
 	unsigned int i = 0;
 
 	temp = h;
-	while (temp)
+	for (i = 0; temp; i++)
 	{
-		if (temp->str == NULL)
-			printf("[%u] %s\n", 0, "(nil)");
-		else
-			printf("[%u] %s\n", temp->len, temp->str);
-		i++;
+		printf("[%u] %s\n", temp->len, temp->str);
 		temp = temp->next;
 	}
 	return (i);
