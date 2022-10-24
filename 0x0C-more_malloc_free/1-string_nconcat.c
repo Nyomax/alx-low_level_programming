ed05ecd0506becc10f534f734fde3162d3cbfead
Maxwell <lifeimax@gmail.com>
2022-10-23 20:52:40 -0700

    file 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,42 +10,30 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
-	char *nul = "";
-	unsigned int x, y;
+	unsigned int x, y, i;
 
-	x = y = 0;
 	if (s1 == NULL)
-		s1 = nul;
-
+		s1 = "";
 	if (s2 == NULL)
-		s2 = nul;
+		s2 = "";
 
-	while (s1[x] != '\0')
-		x++;
+	x = _strlen(s2);
+	y = _strlen(s1);
 
-	while (s2[y] != '\0')
-		y++;
-
-	if (n >= y)
-		n = y;
-
-	str = malloc(sizeof(*str) * n + (x + 1));
+	if (n < y)
+		y = n;
+	str = malloc(x + y + 1);
 	if (str == NULL)
 		return (NULL);
+
+
+	for (i = 0; i < y; i++)
+		*(str + i) = *(s1 + i);
+
+	for (i = 0; i < x; i++)
+		*(str + (i + y)) = *(s2 + i);
+	*(str + (i + y)) = '\0';
 	
-	x = 0;
-	while (s1[x] != '\0')
-	{
-		str[x] = s1[x];
-		x++;
-	}
-	y = 0;
-	while (s2[y] != '\0' && y < n)
-	{
-		str[x] = s2[y];
-		x++;
-		y++;
-	}
-	str[x] = '\0';
+
 	return (str);
 }
