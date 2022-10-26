fb3a8b5cfb6058458c1e58f1ffce3ac960c58765
Maxwell <lifeimax@gmail.com>
2022-10-25 20:23:28 -0700

    file 0

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - Calculate the number of elements.
+ * @h: Pointer to a list.
+ * Return: Integer.
+ */
+size_t list_len(const list_t *h)
+{
+	unsigned int counter = 0;
+
+	while (h)
+	{
+		counter++;
+		h = h->next;
+	}
+	return (counter);
+}
