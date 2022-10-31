b801c01b8b1d58520c53b5988709987a6774e408
Maxwell <lifeimax@gmail.com>
2022-10-30 21:10:19 -0700

    file 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * print_binary - function that prints the binary representation of a number
+ * @b: argument of function pointing to a string of 0 and 1 char
+ * Return: returns the converted number or 0 if
+ * -> there is one or more chars in the string b that is not 0 or 1
+ * -> b is NULL
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int value = 0;
+
+	if (b == NULL)
+		return (0);
+	while (*b != '\0')
+	{
+		value = value << 1;
+		if (*b != '1' && *b != '0')
+			return (0);
+		else if (*b == '1')
+			value = value | 1;
+		b++;
+	}
+	return (value);
+}
