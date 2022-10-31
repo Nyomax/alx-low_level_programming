51c830372f2d60b00764437613a533f1d41dd644
Maxwell <lifeimax@gmail.com>
2022-10-30 22:12:27 -0700

    file 2

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,9 +4,8 @@
  * print_binary - function that prints the binary representation of
  * a number
  * @b: argument of function pointing to a string of 0 and 1 char
- * Return: returns the converted number or 0 if
- * -> tone or more chars in the string b that is not 0 or 1
- * -> b is NULL
+ * Return: returns the converted number or 0 if one
+ * or more chars in the string b that is not 0 or 1 if b is NULL
  */
 unsigned int binary_to_uint(const char *b)
 {
--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -6,7 +6,8 @@
  * @n: bit value in the index
  * @index: is the index, starting from 0 of the bit you
  * want to get
- * Return: the value of the bit at index index or -1 if an error occured
+ * Return: the value of the bit at index index or -1 if an
+ * error occured
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
