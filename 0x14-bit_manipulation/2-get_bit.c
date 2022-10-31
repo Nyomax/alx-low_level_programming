9feafceea12fdb34e90205a518b7b1b7a2e129bc
Maxwell <lifeimax@gmail.com>
2022-10-30 22:17:42 -0700

    file 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,10 +4,8 @@
  * set_bit - function that sets the value of a bit to 1 at
  * a given index.
  * @n: bit value in the index
- * @index: is the index, starting from 0 of the bit you
- * want to get
- * Return: the value of the bit at index index or -1 if an
- * error occured
+ * @index: is the index, starting from 0 of the bit you want
+ * Return: the value of the bit at the given index
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
